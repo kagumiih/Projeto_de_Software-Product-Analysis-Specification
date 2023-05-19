170378963524c6591182a6a9a13ba08b80a52724
Juliane T. Cobiank <72655764+kagumiih@users.noreply.github.com>
2023-05-19 18:08:09 -0300

    Add files via upload

---
+++ projeto/app.py
@@ -0,0 +1,143 @@
+# app.py
+import pyodbc
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cadastro_produto')
+def cadastro_produto():
+    return render_template('cadastro_produto.html')
+
+@app.route('/usuario')
+def usuario():
+    return render_template('cadastro_usuario.html')
+
+@app.route('/estoque')
+def estoque():
+    return render_template('cadastro_estoque.html')
+    #with pyodbc.connect(get_dados_conexao()) as cnx:
+    #    cursor = cnx.cursor()
+
+    #    sql = "SELECT id_produto, nome_produto FROM produto"
+    #    cursor.execute(sql)
+    #    produtos = cursor.fetchall()
+
+    #    cursor.close()
+    #    cnx.close()
+
+    #return render_template('cadastro_estoque.html', produtos=produtos)#
+
+@app.route('/adicionar_produto', methods=['POST'])
+def adicionar_produto():
+    produto = request.form['produto']
+    preco = float(request.form['preco'])
+    tipo = request.form['tipo']
+    marca = request.form['marca']
+    codigo_barras = request.form['codigo_barras']
+    inserir_produto(produto, preco, tipo, marca, codigo_barras)
+    return jsonify({'status': 'OK'})
+
+@app.route('/atualizar_produto', methods=['POST'])
+def atualizar_produto():
+    produto = request.form['produto']
+    preco = float(request.form['preco'])
+    tipo = request.form['tipo']
+    marca = request.form['marca']
+    codigo_barras = request.form['codigo_barras']
+    atualizar_produto_db(produto, preco, tipo, marca, codigo_barras)
+    return jsonify({'status': 'OK'})
+
+@app.route('/cadastrar_usuario', methods=['POST'])
+def cadastrar_usuario():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    cadastrar_usuario_db(usuario, senha)
+    return jsonify({'status': 'OK'})
+
+@app.route('/atualizar_usuario', methods=['POST'])
+def atualizar_usuario():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    atualizar_usuario_db(usuario, senha)
+    return jsonify({'status': 'OK'})
+
+@app.route('/cadastrar_estoque', methods=['POST'])
+def cadastrar_estoque():
+    quantidade = request.form['quantidade']
+    data_entrada = request.form['data-entrada']
+    data_saida = request.form['data-saida']
+    lote = request.form['lote']
+    validade = request.form['validade']
+    id_produto = request.form['id_produto']
+    cadastrar_estoque_db(quantidade, data_entrada, data_saida, lote, validade, id_produto)
+    return jsonify({'status': 'OK'})
+
+def inserir_produto(produto: str, preco: float, tipo: str, marca: str, codigo_barras: str):
+    with pyodbc.connect(get_dados_conexao()) as cnx:
+        cursor = cnx.cursor()
+        inserir_produto_sql = f"""INSERT INTO Produto(nome_produto, valor, tipo, marca, codigo_barras) 
+                                VALUES('{produto}', {preco},'{tipo}','{marca}','{codigo_barras}')"""
+        cursor.execute(inserir_produto_sql)
+        cnx.commit()
+
+def atualizar_produto_db(produto: str, preco: float, tipo: str, marca: str, codigo_barras: str):
+    with pyodbc.connect(get_dados_conexao()) as cnx:
+        cursor = cnx.cursor()
+        atualizar_produto_sql = f"""UPDATE Produto SET nome_produto = '{produto}', valor = {preco} , 
+                                  tipo = '{tipo}', marca = '{marca}', codigo_barras = '{codigo_barras}'
+                                  WHERE nome_produto = '{produto}'"""
+        cursor.execute(atualizar_produto_sql)
+        cnx.commit()
+
+def cadastrar_usuario_db(usuario: str, senha: str):
+    with pyodbc.connect(get_dados_conexao()) as cnx:
+        cursor = cnx.cursor()
+        cadastrar_usuario_sql = f"""INSERT INTO Login(usuario, senha) 
+                                VALUES('{usuario}', '{senha}')"""
+        cursor.execute(cadastrar_usuario_sql)
+        cnx.commit()
+
+def atualizar_usuario_db(usuario: str, senha: str):
+    with pyodbc.connect(get_dados_conexao()) as cnx:
+        cursor = cnx.cursor()
+        atualizar_usuario_sql = f"""UPDATE Login SET usuario = '{usuario}', senha = '{senha}' 
+                                WHERE usuario = '{usuario}' AND  senha = '{senha}'"""
+        
+        cursor.execute(atualizar_usuario_sql)
+        cnx.commit()
+
+def logar(usuario: str, senha: str): 
+    with pyodbc.connect(get_dados_conexao()) as cnx:
+        cursor = cnx.cursor()
+        atualizar_usuario_sql = f"""SELECT * FROM  Login  
+                                WHERE usuario = '{usuario}' AND  senha = '{senha}'"""
+        
+        cursor.execute(atualizar_usuario_sql)
+        cnx.commit()
+
+def cadastrar_estoque_db(quantidade: str, data_entrada: str, data_saida: str, lote: str, validade: str, id_produto: str):
+    with pyodbc.connect(get_dados_conexao()) as cnx:
+            cursor = cnx.cursor()
+            cadastrar_estoque_sql = f"""INSERT INTO Estoque(quantidade_itens, 
+                                                            data_entrada, 
+                                                            data_saida,
+                                                            lote,
+                                                            validade_produto,
+                                                            id_produto) 
+                                    VALUES('{quantidade}', '{data_entrada}',
+                                           '{data_saida}', '{lote}',
+                                           '{validade}', '{id_produto}')"""
+            cursor.execute(cadastrar_estoque_sql)
+            cnx.commit()
+
+def get_dados_conexao() -> str:
+    return ("Driver={SQL Server};"
+            "Server=DESKTOP-JRD01KQ;"
+            "Database=PROJETO_SOFTWARE;")
+
+if __name__ == '__main__':
+    app.run(debug=True)
